cf1405be2bc37ffb3bdff2a7d758caecfe0746da
mario.souza <mario.souza@ilog.com.br>
2021-09-24 15:44:19 -0300

    Portal de apoio a automacao;

---
+++ pages/js/logout.js
@@ -0,0 +1,12 @@
+$(function(){
+	$('#logoutSidebar').on('click', function(){
+		window.top.close();
+        window.open("../index.html");
+	});
+});
+$(function(){
+	$('#logoutMenu').on('click', function(){
+		window.top.close();
+        window.open("../index.html");
+	});
+});
